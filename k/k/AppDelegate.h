0027c59cfc843569c136fc61cfb0c794d8b493bc
赵英奎 <zhaoyingkui@1-2.local>
2015-11-23 11:49:05 +0800

    n

---
+++ k/k/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  k
+//
+//  Created by 赵英奎 on 15/11/23.
+//  Copyright (c) 2015年 赵英奎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ k/k/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  k
+//
+//  Created by 赵英奎 on 15/11/23.
+//  Copyright (c) 2015年 赵英奎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
